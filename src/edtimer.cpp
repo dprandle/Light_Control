faeb74774fd19f007ab47c1a09de0e75d821d841
Daniel <dprandle@alaska.edu>
2019-12-19 14:04:11 -0900

    Added + and - overloads to timespec and changed everything to internally use timespecs

--- src/edtimer.cpp
+++ src/edtimer.cpp
@@ -5,6 +5,7 @@
 #include <edcallback.h>
 #include <edutility.h>
 #include <edlogger.h>
+#include <math>
 
 edtimer::edtimer():
     m_running(false),
@@ -42,16 +43,16 @@ edtimer::cb_mode edtimer::callback_mode()
 
 void edtimer::set_callback(edtimer_callback * cb)
 {
-	if (m_cb != NULL)
+	if (m_cb != nullptr)
 		delete m_cb;
 	m_cb = cb;
-	if (m_cb != NULL)
+	if (m_cb != nullptr)
 		m_cb->timer = this;
 }
 
 double edtimer::callback_delay()
 {
-	return m_cb_delay;
+	return to_ms_(m_cb_delay);
 }
 
 void edtimer::set_callback_mode(cb_mode mode)
@@ -60,9 +61,9 @@ void edtimer::set_callback_mode(cb_mode mode)
 	m_last_cb = m_prev;
 }
 
-void edtimer::set_callback_delay(double secs)
+void edtimer::set_callback_delay(double ms)
 {
-	m_cb_delay = secs;
+	m_cb_delay = from_ms_(ms);
 }
 
 void edtimer::update()
@@ -73,16 +74,16 @@ void edtimer::update()
 	m_prev = m_cur;
 	clock_gettime(CLOCK_MONOTONIC, &m_cur);
 
-	double elpsed = elapsed();
-	double cb_elapsed = elapsed_since_callback();
-	if (((m_cmode == single_shot) && (m_last_cb.tv_nsec == m_start.tv_nsec && m_last_cb.tv_sec == m_last_cb.tv_sec) && (elpsed >= m_cb_delay)) ||
-		((m_cmode == continous_shot) && (cb_elapsed >= m_cb_delay)))
+	timespec elpsed = m_cur - m_start;
+	timespec cb_elapsed = m_cur - m_last_cb;
+	if (((m_cmode == single_shot) && (m_last_cb == m_start) && (elpsed >= m_cb_delay)) ||
+	    ((m_cmode == continous_shot) && (cb_elapsed >= m_cb_delay)))
 	{
 		m_last_cb = m_cur;
 		if (m_cb == nullptr)
 			wlog("No callback set for timer yet callback mode is enabled");
 		else
-			m_cb->exec(); // This must come after - stop calls update()
+			m_cb->exec();
 	}
 }
 
@@ -97,19 +98,37 @@ bool edtimer::running()
 	return m_running;
 }
 
+double edtimer::to_ms_(const timespec & tspec)
+{
+	return double(tspec.tv_sec) * 1000.0 + double(tspec.tv_nsec) * 0.000001;
+}
+
+timespec edtimer::from_ms_(double ms)
+{
+	timespec rtn;
+	double whole, fract;
+	fract = modf(ms*0.001, &whole);
+	rtn.tv_sec = whole;
+	rtn.tv_nsec = fract * 1000000000;
+	return rtn;
+}
+
 double edtimer::dt()
 {
-	return double(m_cur.tv_sec - m_prev.tv_sec + (m_cur.tv_nsec - m_prev.tv_nsec) / 1000000000.0);
+	timespec delta = m_cur - m_prev;
+	return to_ms_(delta);
 }
 
 double edtimer::elapsed()
 {
-	return double(m_cur.tv_sec - m_start.tv_sec + (m_cur.tv_nsec - m_start.tv_nsec) / 1000000000.0);
+	timespec delta = m_cur - m_start;
+	return to_ms_(delta);
 }
 
 double edtimer::elapsed_since_callback()
 {
-	return double(m_cur.tv_sec - m_last_cb.tv_sec + (m_cur.tv_nsec - m_last_cb.tv_nsec) / 1000000000.0);
+	timespec delta = m_cur - m_last_cb;
+	return to_ms_(delta);
 }
 
 bool operator<(const timespec& lhs, const timespec& rhs)
@@ -144,4 +163,20 @@ bool operator>=(const timespec& lhs, const timespec& rhs)
 bool operator>(const timespec& lhs, const timespec& rhs)
 {
 	return !(lhs <= rhs);
+}
+
+timespec operator-(const timespec& lhs, const timespec& rhs)
+{
+	timespec rtn;
+	rtn.tv_sec = lhs.tv_sec - rhs.tv_sec;
+	rtn.tv_nsec = lhs.tv_nsec - rhs.tv_nsec;
+	return rtn;
+}
+
+timespec operator+(const timespec& lhs, const timespec& rhs)
+{
+	timespec rtn;
+	rtn.tv_sec = lhs.tv_sec + rhs.tv_sec;
+	rtn.tv_nsec = lhs.tv_nsec + rhs.tv_nsec;
+	return rtn;
 }
